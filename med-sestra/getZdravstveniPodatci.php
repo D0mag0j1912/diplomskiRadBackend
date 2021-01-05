ada73569bfee158d925a4a9743f4f3120b8421f1
D0mag0j1912 <72351242+D0mag0j1912@users.noreply.github.com>
2021-01-05 15:33:14 +0100

    Inicijalno postavljanje zdr. podataka 2

---
+++ med-sestra/getZdravstveniPodatci.php
@@ -0,0 +1,23 @@
+<?php
+//Importam potrebne klase pomoću autoloadera
+require_once 'C:\wamp64\www\angularPHP\includes\autoloader2.inc.php';
+
+//Dohvaćam liječnički servis
+$servis = new OpciPodatciService();
+
+//Kreiram objekt tipa "Baza"
+$baza = new Baza();
+
+//Konekciju na bazu spremam u varijablu
+$conn = $baza->spojiSBazom();
+
+//Ako je request metoda koja je pozvala ovu skriptu "GET":
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    //Kreiram prazno polje
+    $response = [];
+
+    $response = $servis->dohvatiZdravstvenePodatke();
+
+    echo json_encode($response);
+}
+?>
