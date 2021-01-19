8592825bfeba8ef157bd95c818bd1981818b2114
D0mag0j1912 <72351242+D0mag0j1912@users.noreply.github.com>
2021-01-19 18:26:49 +0100

    Funkcionalnost recepta (početak)

---
+++ recept/importi/getLijekoviDopunskaLista.php
@@ -0,0 +1,24 @@
+<?php
+//Importam potrebne klase pomoću autoloadera
+require_once 'C:\wamp64\www\angularPHP\includes\autoloader3.inc.php';
+
+//Dohvaćam liječnički servis
+$servis = new ImportService();
+
+//Kreiram objekt tipa "Baza"
+$baza = new Baza();
+
+//Konekciju na bazu spremam u varijablu
+$conn = $baza->spojiSBazom();
+
+//Ako je request metoda koja je pozvala ovu skriptu "GET":
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    //Kreiram prazno polje
+    $response = [];
+
+    //Punim polje sa odgovorom funkcije
+    $response = $servis->dohvatiLijekoviDopunskaLista();
+    //Vraćam odgovor frontendu
+    echo json_encode($response);
+}
+?>
