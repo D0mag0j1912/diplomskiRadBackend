04e5e2647e1466f1b25c5e5054b413cd5b8b7111
D0mag0j1912 <72351242+D0mag0j1912@users.noreply.github.com>
2021-01-22 18:59:22 +0100

    Pretraga proizvoda te dohvat cijena

---
+++ recept/cijene/getCijenaLijekOL.php
@@ -0,0 +1,35 @@
+<?php
+//Importam potrebne klase pomoću autoloadera
+require_once 'C:\wamp64\www\angularPHP\includes\autoloader3.inc.php';
+
+//Dohvaćam liječnički servis
+$servis = new ReceptService();
+
+//Kreiram objekt tipa "Baza"
+$baza = new Baza();
+
+//Konekciju na bazu spremam u varijablu
+$conn = $baza->spojiSBazom();
+
+//Ako je request metoda koja je pozvala ovu skriptu "GET":
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    //Kreiram prazno polje
+    $response = [];
+
+    //Ako je sa frontenda poslan parametar pretrage
+    if(isset($_GET['lijek'])){
+        //Dohvaćam vrijednost izabranog lijeka
+        $lijek = mysqli_real_escape_string($conn, trim($_GET['lijek']));
+        //Splitam string da mu uzmem ime i oblik-jačinu-pakiranje
+        $polje = explode(" ",$lijek,2);
+        //Dohvaćam ime lijeka
+        $imeLijek = $polje[0];
+        //Dohvaćam oblik,jačinu i pakiranje
+        $ojp = $polje[1];
+        //Punim polje sa odgovorom funkcije
+        $response = $servis->dohvatiCijenaLijekOL($imeLijek,$ojp);
+        //Vraćam odgovor frontendu
+        echo json_encode($response);
+    }
+}
+?>
---
+++ recept/pretraga/getMagPripravakOsnovnaListaPretraga.php
@@ -0,0 +1,29 @@
+<?php
+//Importam potrebne klase pomoću autoloadera
+require_once 'C:\wamp64\www\angularPHP\includes\autoloader3.inc.php';
+
+//Dohvaćam liječnički servis
+$servis = new ReceptService();
+
+//Kreiram objekt tipa "Baza"
+$baza = new Baza();
+
+//Konekciju na bazu spremam u varijablu
+$conn = $baza->spojiSBazom();
+
+//Ako je request metoda koja je pozvala ovu skriptu "GET":
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    //Kreiram prazno polje
+    $response = [];
+
+    //Ako je sa frontenda poslan parametar pretrage
+    if(isset($_GET['pretraga'])){
+        //Dohvaćam vrijednost liječničke pretrage
+        $pretraga = mysqli_real_escape_string($conn, trim($_GET['pretraga']));
+        //Punim polje sa odgovorom funkcije
+        $response = $servis->dohvatiMagPripravciOsnovnaListaPretraga($pretraga);
+        //Vraćam odgovor frontendu
+        echo json_encode($response);
+    }
+}
+?>
